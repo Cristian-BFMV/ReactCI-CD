aed7ec0f447c4984dce2a4fc0028d5cff247b2ab
Cristian Mendoza <ccamilo.mendoza@udea.edu.co>
2022-10-06 21:41:09 -0500

    Add E2E

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+});

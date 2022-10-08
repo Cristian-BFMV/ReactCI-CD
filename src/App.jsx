4b520e20a470677545efc3f20b5a1e34294b5404
Cristian Mendoza <ccamilo.mendoza@udea.edu.co>
2022-10-08 17:08:18 -0500

    Cloudfront invaldation

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more, deploy test
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">This is a test for cloudfront invalidation</p>
     </div>
   );
 }
